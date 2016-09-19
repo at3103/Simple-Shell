ab8f4b5e2913963259b0d6780a3499b22ddca340
Ashwin Karthik Tamilselvan <at3103@columbia.edu>
2016-09-19 01:22:55 -0400

    Multiple pipe

--- shell.c
+++ shell.c
@@ -8,9 +8,7 @@
 #include <string.h>
 #include <errno.h>
 
-//#define mem_clear(){free(str);free(hist[100]);free(par1);free(temp);}
 
-//char hist[1024][100];
 char *hist[100];//=NULL;
 int hist_count =  -1;
 int pipe_count =  -1;
@@ -36,18 +34,10 @@ int split(char *strtemp, char **temp)
 
 		while ((*str == ' ') || (*str == '\t'))
 			*str++ = '\0';
-		//if(*(str+1)!='\0')
-
-
 
 	}
 	*par = '\0';
-/*
-*	for (i = 0; temp[i]!='\0'; ++i)
-*		printf("Arguments are %sthis\n", temp[i]);
-*/
 
-	//printf("%s\n","out of loop" );/*debug*/
 	free(ch);
 	return 0;
 }
@@ -62,89 +52,18 @@ int split1(char *strtemp, char *temp)
 	char *par = temp;
 	char *str = strtemp;
 
-
-	//printf("%s\n","in loop" );/*debug*/
-
 	while ((*str != ' ') && (*str != '\t') && (*str != '\0'))
 		str++;
-	//printf("%s\n","out while");/*debug*/
 
 	if ((*str == ' ') || (*str == '\t'))
 		*str++ = '\0';
 
-	//temp=str;
 	if (str == '\0')
 		strcpy(temp, " ");
 	else
 		strcpy(temp, str);
-/*	if(*(str+1)!='\0')
-	*par='\0';
-*/
 
 	free(ch);
-	//free(par);
-	//free(str);
-	//free(strtemp);
-	//free(temp);
-	return 0;
-}
-
-
-int getpath(char *str, char *path)
-{
-	char *ch;
-	char *pth;
-	char *comm;
-	size_t l;
-
-	pth = path;
-/*
-*	pth=path+strlen(path);
-*	printf("%s\n","in getpath" );
-*	/*debug*/
-
-/*
-*	*(pth++) = '/';
-*	ch=str;
-*	pth++=str;
-*	while(*ch!=NULL)
-*		*(pth++)=*(ch++);
-*
-*	*(pth)=NULL;
-*	printf("%s\n",path);
-*	while(str!=NULL){
-*		*path++=*str++;
-*		l++;
-*	}
-*/
-
-	l = strlen(str);
-	ch = str+l;
-
-	while (*(ch-1) != '/' && ch != str)
-		ch--;
-
-	if ((*ch) == '/') {
-		comm = ch+1;
-		*pth++ = '/';
-		*pth = '\0';
-	} else if ((ch == str) && *ch != '/') {
-		comm = ch;
-		*pth++ = '.';
-		*pth = '\0';
-
-	} else {
-		comm = ch;
-		*(ch-1) = '\0';
-		strcpy(pth, str);
-		pth++;
-		*pth = '\0';
-	}
-	*(comm+strlen(comm)+1) = '\0';
-
-	//printf("Command is %s\n",comm );/*debug*/
-	//printf("Path is %s\n",path );/*debug*/
-	strcpy(str, comm);
 	return 0;
 }
 
@@ -180,7 +99,6 @@ int display_history(void)
 		return 0;
 	}
 
-	//printf("\n\n" );
 	for (i = 0; i <= hist_count; i++)
 		printf("%d %s\n", i, hist[i]);
 	return 0;
@@ -190,7 +108,6 @@ int clear_history(void)
 {
 	hist_count =  -1;
 	free(hist[100]);
-	//printf("%s\n","history cleared!!" ); /*debug*/
 	return 0;
 }
 
@@ -200,26 +117,23 @@ int compute_pipe(char *str)
 
 	int i;
 
-
 	for (temp = strtok(str, "|"); temp != NULL; temp = strtok(NULL, "|")) {
 		pipe_count++;
 		strpipe[pipe_count] = (char *)malloc(sizeof(temp));
 		strcpy(strpipe[pipe_count], temp);
 	}
 
-	// temp=strtok(NULL,'\0');
-	//pipe_count++;
-	//strtemp[pipe_count]=(char*)malloc(sizeof(temp));
-	//strcpy(strtemp[pipe_count],temp);
-	for (i = 0; i <= pipe_count; ++i)
-		printf("%d%s\n", i, strpipe[i]);
-	printf("The count is %d\n", pipe_count);
-	//exit(0);
-
 	return 0;
 }
 
+int close_pipe(int *pfd,int i,int f)
+{
+	int l=0;
 
+	for (l = 0; l <= pipe_count; ++l) {
+		close(pfd[l]);
+	}
+}
 
 int main(int argc, char const *argv[])
 {
@@ -261,13 +175,9 @@ int main(int argc, char const *argv[])
 	/*For continuos prompts*/
 	for (;;) {
 		flag = 0;
-		/**path=' ';*/
 		if (hflag == 0 && pflag == 0) {
 			printf("$");
 			getline(&str, &str_size, stdin);
-/*			printf("%s\n",str );
-*			printf("%zu\n",str_size );
-			*/
 		}
 
 		str_len = strlen(str);
@@ -287,7 +197,15 @@ int main(int argc, char const *argv[])
 				printf("%d\n", pipe_count);
 				pfd = (int *)malloc((pipe_count+1)*sizeof(int));
 				pipe_curr = 0;
-				pipe(pfd);
+				
+				for (i = 0; i < pipe_count; ++i)
+				{
+					if(pipe(pfd+(2*i))== -1){
+						perror("pipe");
+						exit(EXIT_FAILURE);
+					}
+				}
+				
 				pflag = 1;
 			}
 /*
@@ -297,7 +215,6 @@ int main(int argc, char const *argv[])
 
 			strcpy(str, strpipe[pipe_curr]);
 
-			/* code */
 		}
 		str1 = (char *)malloc(str_len*sizeof(char));
 		par1 = (char *)malloc(str_len*sizeof(char));
@@ -317,7 +234,7 @@ int main(int argc, char const *argv[])
 		split(str, par);
 		//split1(str1,par1);
 		strcpy(temp, str1);
-		//getpath(temp,path);
+
 
 		//printf("Command in main is %s\n",temp );/*debug*/
 		//printf("Parameters in main is %s\n",par1 );/*debug*/
@@ -378,86 +295,52 @@ int main(int argc, char const *argv[])
 				exit(1);
 			}
 
-			if (pid == 0) {
-				if (pflag == 1) {
-					printf("%s\n", "Entered");
-					if (pipe_curr < pipe_count
-						&& pipe_curr != 0) {
-						printf("%s\n", "Entered2");
-						dup2(pfd[pipe_curr]-1, 0);
-						dup2(pfd[pipe_curr+1], 1);
-						for (l = 0;
-							l <= pipe_count; ++l)
-							// if(l==pipe_curr+1
-							//|| l==pipe_curr-1)
-							//	continue;
-							close(pfd[l]);
-					} else if (pipe_curr == 0) {
-						printf("%s\n", "Entered3");
-						dup2(pfd[pipe_curr+1], 1);
+			else if (pid == 0 && pflag == 1) {
+				printf("%s\n", "Entered");
+				if (pipe_curr < pipe_count
+					&& pipe_curr != 0) {
+					printf("%s\n", "Entered2");
+					dup2(pfd[pipe_curr-1], 0);
+					//read(pfd[pipe_curr-1]);
+					dup2(pfd[pipe_curr+1], 1);
+					close_pipe(pfd,pipe_curr,0);
+					//close_pipe(pfd,pipe_curr,0);
+					// close(pfd[pipe_curr+1]);
+					// close(pfd[pipe_curr-1]);
+
+				} else if (pipe_curr == 0) {
+					printf("%s\n", "Entered3");
+					dup2(pfd[pipe_curr+1], 1);
+					close_pipe(pfd,pipe_curr,1);
 
 
-						for (l = 0;
-							l <= pipe_count; ++l)
-							// if(l==pipe_curr+1)
-							//	continue;
-							close(pfd[l]);
-					} else {
-						dup2(pfd[pipe_curr]-1, 0);
-						printf("%s\n", "Entered4");
-						for (l = 0;
-							l <= pipe_count; ++l)
-							// if(l==pipe_curr-1)
-							//	continue;
-							close(pfd[l]);
+				} else {
+					dup2(pfd[pipe_curr-1], 0);
+					printf("%s\n", "Entered4");
+					close_pipe(pfd,pipe_curr,-1);
+				}
+				printf("The path is %s\n", str);/*debug*/
+				printf("The command is %s\n", temp);/*debug*/
 
-					}
-
-
+				printf("The arg are %s\n", par[1]);/*debug*/
+				if (execv(str, par) ==  -1) 
+					printf("error: %s\n", strerror(errno));
+				return 0;
 				}
 
-				// printf("CHILD\n");/*debug*/
-
+			else if (pid == 0 && pflag==0)
+			{
 				printf("The path is %s\n", str);/*debug*/
 				printf("The command is %s\n", temp);/*debug*/
 
 				printf("The arg are %s\n", par[1]);/*debug*/
 
-/*				  for (i = 0; par[i]!=NULL; ++i){
-*				  printf("First argument is %s ", par[i]);
-*				  }
-*/
-				//printf("\n");
-				//execl("/bin/ls","ls",".",0);
-
-				if (execv(str, par) ==  -1) {
+				if (execv(str, par) ==  -1) 
 					printf("error: %s\n", strerror(errno));
-					//mem_clear();
-
-				}
-
-/*
-*				if(strcmp(par1,"\0")==0) {
-*					execl(str,temp,0);
-*				} else
-*					execl(str,temp,par1,0);
-*/
 				return 0;
-
-
-
 			}
 
 			else {
-
-/*
-*				if (pflag==1)
-*				{
-*				dup2(pdf[pipe_curr],0);
-*
-*				for ( l = 0; l <= pipe_count; ++l)
-*					close(pdf[l])
-*/
 
 
 				if (pflag == 1 && pipe_curr < pipe_count) {
@@ -470,16 +353,15 @@ int main(int argc, char const *argv[])
 					pipe_curr++;
 					printf("%s%d\n", "Inc", pipe_curr);
 				}
-				if (pflag == 1 && pipe_curr == pipe_count+1) {
+				if (pflag == 1 
+					&& pipe_curr == pipe_count+1) {
 
 					pflag = 0;
-					dup2(0, 0);
-					dup2(1, 1);
+					//dup2(0, 0);
+					//dup2(1, 1);
 					printf("%s\n", "Finished");
 					pipe_curr = 0;
-					for (l = 0; l <= pipe_count; ++l)
-						close(pfd[l]);
-					//mem_clear();
+					close_pipe(pfd,-1,0);
 					pipe_count =  -1;
 					free(strpipe[64]);
 					free(pfd);
@@ -487,21 +369,11 @@ int main(int argc, char const *argv[])
 				}
 				while (wait(&status) != pid)
 					;
-				//mem_clear();
 
 			}
 		}
 
-
 	}
-
-
 	mem_clear();
-
-	// free(str);
-	// free(hist[100]);
-	// free(par1);
-	//free(temp);
-	//free(&str_size);
 	return 0;
 }
